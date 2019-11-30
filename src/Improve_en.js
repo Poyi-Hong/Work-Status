51e04e9d36e45d0cee3233c3e84052ed776fb080
Poyi Hong <9406088@gmail.com>
2019-11-30 21:09:14 +0800

    clear

--- src/Improve_en.js
+++
@@ -1,5 +0,0 @@
-﻿$("#typhoonstatuscard").append(TY_LIST_1["E"])
-$('.accordion-toggle').replaceWith('<div>' + $(".accordion-toggle").html() + '</div>')
-$('h4').replaceWith('<h5 class="card-title">' + $("h4").html() + '</h5>').addClass("card-title")
-$('.panel-collapse').replaceWith('<div>' + $(".panel-collapse").html() + '</div>')
-$("#tyimg").attr('src', 'https://www.cwb.gov.tw/' + '/Data/' + DataPath + 'Download_PTA_' + TY_DataTime + '_enus.png');
